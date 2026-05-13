1f25885b3f7ade2b08e81ef2443364630c210e58
Ignacio Perez <ignacio@feuer.me>
2026-05-13 14:17:33 -0300

    chore(release): v0.5.3

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
         # Import default.nix with crane support
         dbflux = import ./default.nix {
           inherit pkgs craneLib;
-          version = "0.5.2";
+          version = "0.5.3";
         };
 
         # Main package built with crane
